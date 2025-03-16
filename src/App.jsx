6c3f2c0ed6faf725eb9d36707861290db2fda9be
Wutt Saeng <wuttforwork@gmail.com>
2025-03-16 13:38:45 +0700

    Setup project environment

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-5xl text-center p-18'>Vite react project with Tailwind css v4 and shadcn ui</h1>
+    <Button variant="default">Button</Button>
+
+    </>
+  )
+}
+
+export default App
